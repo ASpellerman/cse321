278721ba3b8b8d134fc8ef273ae0d569d1721101
ASpellerman <143441491+ASpellerman@users.noreply.github.com>
2024-12-04 17:08:00 -0500

    Add files via upload

---
+++ finalCode/finalCode.ino
@@ -0,0 +1,67 @@
+#include <Servo.h>
+int motorControl = 8;
+int numSensors = 6;
+int sens[6] = {1,2,3,4,5,6};
+int sensAngles[6] = {194, 222, 263, 298, 317, 357};
+float sum = 0;
+int numAng = 0;
+float angle = 0;
+float currentAngle = 0;
+float delayAmount = 0;
+float delayScaler = 0;
+float sameAngle = 0.1;
+Servo motor;
+
+void setup() {
+  //sets up the motor with the library functionality
+  Serial.begin(9600);
+  motor.attach(motorControl);
+  pinMode(motorControl, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  //input for the sensors
+  for(int i = 0; i < numSensors; i++){
+    pinMode(sens[i], INPUT);
+  }
+}
+
+void loop() {
+  //adds up the angles of the sensors that are detecting motion
+  for(int i = 0; i < numSensors; i++){
+    if(digitalRead(sens[i])){
+      sum += sensAngles[i];
+      numAng++;
+    }
+  }
+
+  //if no sensors are active, then just quit
+  if(numAng == 0){
+    delay(500);
+    return;
+  }
+  
+  //calculates the average angle of the sensors that are detecting motion 
+  //(their FOV is big enough that a person appears on several at a time)
+  angle = sum / numAng;
+  //this checks if the angle is more or less the same, and does not rotate if so.
+  if(abs(angle - currentAngle) > sameAngle){
+    delay(500);
+    return;
+  }
+
+  //delays while the servo is rotating
+  motor.write(angle);
+  delayAmount = (abs(currentAngle - angle) * delayScaler);
+  delay(delayAmount);
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ motor_test/motor_test.ino
@@ -0,0 +1,120 @@
+//#include <Wire.h>
+#include <Servo.h>
+//#include "Adafruit_OV7670-master/src/image_ops.h"  
+//#include "Adafruit_OV7670-master/src/Adafruit_OV7670.h"  
+//#include "Adafruit_OV7670-master/src/ov7670.h"  
+//#include "Adafruit_ILI9341.h"
+int motorControl = 8;
+Servo motor;
+//int leftRotation = 0;
+//int rightRotation = 0;
+
+void setup() {
+  //motor things
+  Serial.begin(9600);
+  motor.attach(motorControl);
+  pinMode(motorControl, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  //camera things
+  //pins that change value for this to work: D12, D3, D2, A4, A5
+  //D12 is PCLK
+  //D3 is XCLK
+  //D2 is VSYNC
+  //A4 is SIOD and through 20K SIOC????
+  //A5 is SIOC and through 20k SIOD
+  //input pins: A0, A1, A2, A3, D4, D5, D6, D7
+
+}
+
+void loop() {
+  motor.write(180);
+  delay(2000);
+  motor.write(90);
+  delay(2000);
+}
+void processImage(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct pixel{
+//  u_int red;
+//  u_int green;
+//  u_int blue;   Adafruit_ZeroDMA.h = Adafruit_ZeroDMA.h
+//};
+//pixel[][] image;
+
+/*void rotateFan(bool isRight){
+  if(isRight){
+    analogWrite(motorControl, rightRotation);
+    delay(1000);
+  }else{
+    analogWrite(motorControl, leftRotation);
+    delay(1000);
+  }
+}
+void reduceImage(int startX, int startY, int endX, int endY){
+  //how many chunks
+  int chunkX = startX / endX;
+  int chunkY = startY / endY;
+  //size of each chunk
+  int deltaX = startX / chunkX;
+  int deltaY = startY / chunkY;
+  int currentXChunk = 0;
+  int currentYChunk = 0;
+  pixel[][] newImage;
+  for(int currentXChunk = 0; currentXChunk < chunkX; currnetXChunk++){
+    u_long redSum = 0;
+    u_long greenSum = 0;
+    u_long blueSum = 0;
+    for(int currentXChunk = 0; currentXChunk < chunkX; currnetXChunk++){
+      for(int x = 0; x < chunkX x++){
+        for(int y = 0; y < chunkY; y++){
+          blueSum += image[x + (currentXChunk * deltaX)][y + (currentYChunk * deltaY].blue;
+          greenSum += image[x + (currentXChunk * deltaX)][y + (currentYChunk * deltaY].green;
+          redSum += image[x + (currentXChunk * deltaX)][y + (currentYChunk * deltaY].red;
+        }
+      }
+      redSum = redSum / (((u_long)deltaX) * deltaY);
+      greenSum = greenSum / (((u_long)deltaX) * deltaY);
+      blueSum = blueSum / ((((u_long)deltaX) * deltaY);
+      newImage[][] = new pixel(redSum, greenSum, blueSum);
+    }
+  }
+  image = newImage;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
